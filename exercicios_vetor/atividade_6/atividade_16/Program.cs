e6c17d61a4e751a889b732f2621ded86045a73c8
Lucas Araujo <lucasbackar@gmail.com>
2023-05-02 11:24:24 -0300

    atualizacao

--- exercicios_vetor/atividade_6/atividade_16/Program.cs
+++ exercicios_vetor/atividade_6/atividade_16/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //16. Ler um número N qualquer menor ou igual a 50 e apresentar o valor obtido da 
+            //multiplicação sucessiva de N por 3 enquanto o produto for menor que 250
+            //(N * 3; N * 3 * 3; N * 3 * 3 * 3; etc).
+
+            int num =0;
+            
+            while (num <= 250)
+            {
+                num++;
+            }
+
+
         }
     }
 }
